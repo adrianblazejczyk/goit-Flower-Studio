c6378859df1491ff0680a9d491f5e81ab7cf34ff
adrianblazejczyk <121712995+adrianblazejczyk@users.noreply.github.com>
2023-04-06 04:20:53 +0000

    Deploying to gh-pages from @ adrianblazejczyk/goit-flower-studio@03781e086cad643568b365cc5145d236f42fc1d0 🚀

---
+++ index.5adbe6c2.js
@@ -0,0 +1,2 @@
+var modal=document.getElementById("myModal2"),btn=document.getElementById("free-banner"),span=document.getElementsByClassName("close-banner")[0];btn.onclick=function(){modal.style.display="block"},span.onclick=function(){modal.style.display="none"},window.onclick=function(n){n.target==modal&&(modal.style.display="none")};
+//# sourceMappingURL=index.5adbe6c2.js.map
---
+++ index.cdaecb61.js
@@ -0,0 +1,2 @@
+var modal=document.getElementById("myModal1"),btn=document.getElementById("free"),span=document.getElementsByClassName("close")[0];btn.onclick=function(){modal.style.display="block"},span.onclick=function(){modal.style.display="none"},window.onclick=function(n){n.target==modal&&(modal.style.display="none")};
+//# sourceMappingURL=index.cdaecb61.js.map
